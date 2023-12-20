8e2f88c9e107a280a394db0b8257db6ee720b91f
йцу <>
2023-12-20 13:18:03 +0300

    AutoCommit_20 декабря 2023 г. 13:18:03_SibNout2023

---
+++ 000_Test.py
@@ -0,0 +1,8 @@
+import os
+if os.path.abspath(os.curdir) == os.path.dirname(os.path.realpath(__file__)) :
+	os.system("start notepad++ "+__file__)
+	exit()
+print ("Путь к текущей дирректории:",os.path.abspath(os.curdir))
+print ("Путь к исполняемому файлу:", os.path.dirname(os.path.realpath(__file__)))
+print ("Привет Мирр")
+test_text = input(" Для завершения программы нажмите Enter: ")
