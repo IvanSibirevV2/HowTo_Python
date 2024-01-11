e3410ffaefa385c1eb28226c4ef52ada642e519c
йцу <>
2024-01-11 09:31:42 +0300

    AutoCommit_11 января 2024 г. 9:31:42_SibNout2023

---
+++ 000_6_For.py
@@ -0,0 +1,28 @@
+import os
+if os.path.abspath(os.curdir) == os.path.dirname(os.path.realpath(__file__)) :
+	os.system("start notepad++ "+__file__)
+	exit()
+##########################################################
+#for
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w, len(w))
+##########################################################
+# Create a sample collection
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# Strategy:  Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+#list
+for i in range(5):
+    print(i)
+##########################################################
+test_text = input(" Для завершения программы нажмите Enter: ")
--- 000_7_ВекторныеОперации.py
+++ 000_7_ВекторныеОперации.py
@@ -3,26 +3,9 @@ if os.path.abspath(os.curdir) == os.path.dirname(os.path.realpath(__file__)) :
 	os.system("start notepad++ "+__file__)
 	exit()
 ##########################################################
-def Test_1():
-	the_world_is_flat = True
-	#the_world_is_flat = False
-	if the_world_is_flat:
-		print("the_world_is_flat = True")
-	else :
-		print('the_world_is_flat = False')
-#Test_1()
+
 ##########################################################
-def Test_2():
-	x = int(input("Please enter an integer: "))
-	if x < 0:
-		x = 0
-		print('Negative changed to zero')
-	elif x == 0:
-		print('Zero')
-	elif x == 1:
-		print('Single')
-	else:
-		print('More')
-#Test_2()
+#Векторная операция сложения элементов массива
+print(sum(range(4)))
 ##########################################################
 test_text = input(" Для завершения программы нажмите Enter: ")
