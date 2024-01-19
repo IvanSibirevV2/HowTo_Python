a1f934fa3c509fbd69cd56f1e2b196f512669271
йцу <>
2024-01-19 22:21:50 +0300

    AutoCommit_19 января 2024 г. 22:21:50_SibNout2023

--- 000_9.py
+++
@@ -1,8 +0,0 @@
-import os
-if os.path.abspath(os.curdir) == os.path.dirname(os.path.realpath(__file__)) :
-	os.system("start notepad++ "+__file__)
-	exit()
-##########################################################
-#4.8.2. Keyword Arguments
-##########################################################
-test_text = input(" Для завершения программы нажмите Enter: ")
