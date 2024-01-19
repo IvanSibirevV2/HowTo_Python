dca22ce064b6bf94669acdbaedd7a2a532214c3c
йцу <>
2024-01-19 20:23:39 +0300

    AutoCommit_19 января 2024 г. 20:23:39_SibNout2023

--- 000_8_Functions.py
+++ 000_8_Functions.py
@@ -3,6 +3,7 @@ if os.path.abspath(os.curdir) == os.path.dirname(os.path.realpath(__file__)) :
 	os.system("start notepad++ "+__file__)
 	exit()
 ##########################################################
+print('--000--')
 def fib(n):    # write Fibonacci series up to n
 	"""Print a Fibonacci series up to n."""
 	a, b = 0, 1
@@ -14,6 +15,7 @@ def fib(n):    # write Fibonacci series up to n
 fib(2000)
 print(fib)
 ##########################################################
+print('--001--')
 def fib2(n):  # return Fibonacci series up to n
 	"""Return a list containing the Fibonacci series up to n."""
 	result = []
@@ -22,9 +24,47 @@ def fib2(n):  # return Fibonacci series up to n
 		result.append(a)    # see below
 		a, b = b, a+b
 	return result
-
 f100 = fib2(100)    # call it
 print(f100)
 #4.8.1. Default Argument Values¶
+##########################################################
+print('--002--')
+#сначала заданны просто параметры
+#потом заданы параметры по умолчанию
+def fib3(var1,var2=1,var3='212'):  
+	print('var1=',var1)
+	print('var2=',var2)
+	print('var3=',var3)
+fib3('Привет мир')
+fib3('Привет мир',var2=None)
+##########################################################
+print('--003--')
+i = 5
+def f(arg=i):
+    print(arg)
+i = 6
+f()
+##########################################################
+#Important warning: The default value is evaluated only once. This makes a difference when the default is a mutable object such as a list, dictionary, or instances of most classes. For example, the following function accumulates the arguments passed to it on subsequent calls:
+print('--004--')
+def f(a, L=[]):
+	L.append(a)
+	return L
+print(f(1))
+print(f(2))
+print(f(3))
+##########################################################
+print('--005--')
+#If you don’t want the default to be shared between subsequent calls, you can write the function like this instead:
+def f(a, L=None):
+	if L is None:
+		L = []
+	L.append(a)
+	return L
+print(f(1))
+print(f(2))
+print(f(3))
+##########################################################
+#4.8.2. Keyword Arguments
 ##########################################################
 test_text = input(" Для завершения программы нажмите Enter: ")
