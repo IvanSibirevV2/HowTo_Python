5403528093b586d3066ed10ebfaedcfad84b245c
йцу <>
2024-01-20 09:05:41 +0300

    AutoCommit_20 января 2024 г. 9:05:41_SibNout2023

--- Untitled —_000_10.bat
+++ Untitled —_000_10.bat
@@ -32,20 +32,6 @@ echo ћногострочный %NL%^
 
 
 
-6.1. More on Modules
-6.1.1. Executing modules as scripts
-6.1.2. The Module Search Path
-6.1.3. УCompiledФ Python files
-6.2. Standard Modules
-6.3. The dir() Function
-6.4. Packages
-6.4.1. Importing * From a Package
-6.4.2. Intra-package References
-6.4.3. Packages in Multiple Directories
-7. Input and Output
-7.1. Fancier Output Formatting
-7.1.1. Formatted String Literals
-7.1.2. The String format() Method
 7.1.3. Manual String Formatting
 7.1.4. Old string formatting
 7.2. Reading and Writing Files
