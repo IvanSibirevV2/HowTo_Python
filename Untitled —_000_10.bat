ea8d23092b04b08815f517e6679cc091a4e62579
йцу <>
2024-01-20 09:13:40 +0300

    AutoCommit_20 января 2024 г. 9:13:39_SibNout2023

--- Untitled —_000_10.bat
+++ Untitled —_000_10.bat
@@ -32,20 +32,6 @@ echo Многострочный %NL%^
 
 
 
-8.9. Raising and Handling Multiple Unrelated Exceptions
-8.10. Enriching Exceptions with Notes
-9. Classes
-9.1. A Word About Names and Objects
-9.2. Python Scopes and Namespaces
-9.2.1. Scopes and Namespaces Example
-9.3. A First Look at Classes
-9.3.1. Class Definition Syntax
-9.3.2. Class Objects
-9.3.3. Instance Objects
-9.3.4. Method Objects
-9.3.5. Class and Instance Variables
-9.4. Random Remarks
-9.5. Inheritance
 9.5.1. Multiple Inheritance
 9.6. Private Variables
 9.7. Odds and Ends
