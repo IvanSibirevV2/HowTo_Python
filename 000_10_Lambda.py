ab3b9027b012b8b94f2c99502355344b34617ecf
йцу <>
2024-02-05 13:52:15 +0300

    AutoCommit_5 февраля 2024 г. 13:52:15_SibNout2023

--- 000_10_Lambda.py
+++ 000_10_Lambda.py
@@ -30,9 +30,16 @@ print("[(x, x**2) for x in range(6)]","\n    ",[(x, x**2) for x in range(6)])
 # flatten a list using a listcomp with two 'for'
 vec = [[1,2,3], [4,5,6], [7,8,9]]
 print("vec = [[1,2,3], [4,5,6], [7,8,9]]","\n    ",vec)
-print("[num for elem in vec for num in elem]","\n    ",[num for elem in vec for num in elem])
+print("!!!![num for elem in vec for num in elem]","\n    ",[num for elem in vec for num in elem])
 ##########################################################
 from math import pi
 print("[str(round(pi, i)) for i in range(1, 6)]","\n    ",[str(round(pi, i)) for i in range(1, 6)])
+##########################################################
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+print("matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]","\n    ",matrix)
+print("[[row[i] for row in matrix] for i in range(4)]","\n    ",[[row[i] for row in matrix] for i in range(4)])
+##########################################################
+##########################################################
+##########################################################
 ##########################################################
 test_text = input(" Для завершения программы нажмите Enter: ")
