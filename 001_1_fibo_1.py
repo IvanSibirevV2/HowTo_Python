6c3fe489306cce26c004f7ef17312ea7f3611e0a
йцу <>
2024-05-17 11:52:01 +0300

    AutoCommit_17 мая 2024 г. 11:52:01_SibNout2023

--- 001_1_fibo_1.py
+++ 001_1_fibo_1.py
@@ -6,6 +6,12 @@ if os.path.abspath(os.curdir) == os.path.dirname(os.path.realpath(__file__)) :
 import fiboMod
 print(fiboMod.fib(1000))
 print(fiboMod.fib2(100))
+##
+# dir - интроспекция
+print(dir(fiboMod))
+import sys
+print(dir(sys))
+print(dir())
 ##########################################################
 
 test_text = input(" Для завершения программы нажмите Enter: ")
