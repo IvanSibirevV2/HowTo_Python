31afb5a3d9b9cb481c9fdc81daf665f8407e4263
йцу <>
2024-05-29 22:47:43 +0300

    AutoCommit_29 мая 2024 г. 22:47:42_SibNout2023

---
+++ Examples_BaseUpLevel/000_0_.py
@@ -0,0 +1,7 @@
+import os
+if os.path.abspath(os.curdir) == os.path.dirname(os.path.realpath(__file__)) :
+	os.system("start notepad++ "+__file__)
+	exit()
+##########################################################
+##########################################################
+test_text = input(" Для завершения программы нажмите Enter: ")
